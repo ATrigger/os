563e1d7fd7c8ba032302755d99ba0c865b0a1ed8
Atrigger <mazahakaforever@ya.ru>
2016-02-29 23:39:10 +0300

    CAT

--- HW1/cat.c
+++ HW1/cat.c
@@ -12,19 +12,14 @@
 
 int result;
 
-void scaninput(char *argv[]);
 void raw(int fd);
 
 int main(int argc,char *argv[]){
-    scaninput(argv);
-    return result;
-}
-void scaninput(char *argv[]){
     int i=1; //multiple file concatenation input
     char *path;
     FILE *fp;
 
-    while((path=argv[i]) != NULL || i==1){
+    while((path= argv[i]) != NULL || i == 1){
         int fd;
         if(path == NULL)
             fd=STDIN_FILENO;
@@ -41,24 +36,20 @@ void scaninput(char *argv[]){
         if(path == NULL) break;
         i++;
     }
+    return result;
 }
 
 void raw(int fd){
     int off, wfd;
     ssize_t nr, nw;
-    static size_t bsize;
     static char *buf = NULL;
-    struct stat sbuf;
 
     wfd = fileno(stdout);
     if (buf == NULL) {
-        if (fstat(wfd, &sbuf))
-            err(1, "%d", fd);
-        bsize = ((sbuf.st_blksize> 1024)?(sbuf.st_blksize):(1024));
-        if ((buf = malloc(bsize)) == NULL)
+        if ((buf = malloc(1024)) == NULL)
             err(1, "buffer");
     }
-    while ((nr = read(fd, buf, bsize)) > 0)
+    while ((nr = read(fd, buf, 1024)) > 0)
         for (off = 0; nr; nr -= nw, off += nw)
             if ((nw = write(wfd, buf + off, (size_t)nr)) < 0)
                 err(1, "stdout");
