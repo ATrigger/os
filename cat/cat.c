3ac147ea5d68f357b5f0d70b2c901fa900b7ccb8
Atrigger <mazahakaforever@ya.ru>
2016-03-10 15:26:01 +0300

    no reading from files

--- cat/cat.c
+++ cat/cat.c
@@ -3,57 +3,41 @@
 //
 
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <fcntl.h>
-#include <err.h>
-#include <malloc.h>
-#include <sys/stat.h>
 
-int result;
+const size_t BUF_SIZE = 1024;
 
-void raw(int fd);
-
-int main(int argc,char *argv[]){
-    int i=1; //multiple file concatenation input
-    char *path;
-    FILE *fp;
-
-    while((path= argv[i]) != NULL || i == 1){
-        int fd;
-        if(path == NULL)
-            fd=STDIN_FILENO;
-        else {
-            fd = open(path,O_RDONLY);
-        }
-        if(fd < 0)
-            result = 1;
-        else {
-            raw(fd);
-            if(fd != STDIN_FILENO)
-                close(fd);
-        }
-        if(path == NULL) break;
-        i++;
+ssize_t read_for(int fd, void *buf, size_t size) {
+    size_t count = 0;
+    ssize_t ret;
+    while ((ret = read(fd, buf + count, size - count)) > 0) {
+        count += ret;
     }
-    return result;
+    if(ret == -1) return -1;
+    return count;
 }
 
-void raw(int fd){
-    int off, wfd;
-    ssize_t nr, nw;
-    static char *buf = NULL;
 
-    wfd = fileno(stdout);
-    if (buf == NULL) {
-        if ((buf = malloc(1024)) == NULL)
-            err(1, "buffer");
+ssize_t write_for(int fd, void *buf, size_t size) {
+    size_t written = 0;
+    ssize_t ret;
+    while (written < size && (ret = write(fd, buf + written, size - written)) > 0) {
+        written += ret;
     }
-    while ((nr = read(fd, buf, 1024)) > 0)
-        for (off = 0; nr; nr -= nw, off += nw)
-            if ((nw = write(wfd, buf + off, (size_t)nr)) < 0)
-                err(1, "stdout");
-    if (nr < 0) {
-        result = 1;
+    if(ret == -1) return -1;
+    return written;
+}
+
+int main() {
+    char buf[BUF_SIZE];
+    while (1) {
+        ssize_t count = read_for(STDIN_FILENO, buf, BUF_SIZE);
+        if (count <= 0)
+            return count;
+        ssize_t write_rs = write_for(STDOUT_FILENO, buf, count);
+        if (write_rs == -1)
+            return errno;
     }
+    return 0;
 }
