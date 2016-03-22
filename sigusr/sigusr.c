03b8a448ba33346d6f45f43c7aa610849083faf8
ATrigger <mazahakaforever@ya.ru>
2016-03-22 13:48:18 +0300

    fix

---
+++ sigusr/sigusr.c
@@ -0,0 +1,12 @@
+#include <signal.h>
+#include <stdio.h>
+void handler(int sig, siginfo_t* info, void* vp) { 
+    printf("got sigusr from %d\n",info->si_pid);
+} 
+
+int main(){
+    struct sigaction action;
+    action.sa_sigaction = handler;  
+    sigaction(SIGUSR1,&action,0);
+    sleep(20); 
+}
