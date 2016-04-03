81684f26d600b3d83ff3cfde6a25b8cf01bf3c7e
Atrigger <mazahakaforever@ya.ru>
2016-04-03 18:45:33 +0300

    sigusr

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -1,12 +1,23 @@
 #include <signal.h>
 #include <stdio.h>
-void handler(int sig, siginfo_t* info, void* vp) { 
-    printf("got sigusr from %d\n",(int)((pid_t)info->si_pid));
-} 
+#include <zconf.h>
+#include <bits/siginfo.h>
+#include <stdlib.h>
+
+void handler(int sig, siginfo_t* info, void* vp) {
+    printf("SIGUSR%d from %d\n",(sig==SIGUSR1?(1):(2)),info->si_pid);
+    exit(1);
+}
 
 int main(){
     struct sigaction action;
-    action.sa_sigaction = handler;  
+    action.sa_sigaction = handler;
+    action.sa_flags=SA_SIGINFO;
+    sigemptyset(&action.sa_mask);
+    sigaddset(&action.sa_mask,SIGUSR1);
+    sigaddset(&action.sa_mask,SIGUSR2);
     sigaction(SIGUSR1,&action,0);
-    sleep(20); 
+    sigaction(SIGUSR2,&action,0);
+    sleep(20);
+    printf("No signals were caught\n");
 }
