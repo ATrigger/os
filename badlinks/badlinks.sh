4f942bb761a32c4e65c7809e54a6a6a013793f76
Your name <you@example.com>
2016-05-16 21:19:41 +0300

    fixed empty folders

--- badlinks/badlinks.sh
+++ badlinks/badlinks.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 recurse() {
-	for element in $1/*; do
-		if [ -h "$element" -a ! -e "$element" -a `stat --format=%Y $element` -le $(( `date +%s` - 604800 )) ] 
-    			    then echo "$element"
-		elif [ -d "$element" ] 
-			then recurse $element
-		fi
-	done
+    if [ "$(ls -A $1)" ]; then
+        for element in $1/*; do
+            if [ -h "$element" -a ! -e "$element" -a `stat --format=%Y $element` -le $(( `date +%s` - 604800 )) ] 
+                        then echo "$element"
+            elif [ -d "$element" ] 
+                then recurse $element
+            fi
+        done
+    fi
 }
 recurse $1
